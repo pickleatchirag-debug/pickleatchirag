914ef497743761ef5116b6046ae36b3c29ace4e9
pickleatchirag-debug <pickleatchirag@gmail.com>
2026-06-11 23:38:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,11 +7,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(__dirname));
 
-// Replace this with your deployed Google Web App Executable Link URL
-const GOOGLE_SCRIPT_URL = "// 📄 server.js (Lines 8-11)
-
-// Replace this with your deployed Google Web App Executable Link URL
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycby_elXprUxfCPl1WYiPx2gc6TWpohNY-osHhfGgxeZBacn1vimm433n7sHUx2AvuVvHtg/exec";";
+// Deployed Google Web App Executable Link URL
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycby_elXprUxfCPl1WYiPx2gc6TWpohNY-osHhfGgxeZBacn1vimm433n7sHUx2AvuVvHtg/exec";
 
 // Local structural session token memory cache array
 let REGISTERED_USERS = [];
